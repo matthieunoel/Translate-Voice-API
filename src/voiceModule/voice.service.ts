e4355eec2c88369b844351d7245b424ecac33826
MattUnicornBlue <matthieu.noel@epsi.fr>
2021-01-28 15:50:50 +0100

    Notes sur l'idée de base.

--- src/voiceModule/voice.service.ts
+++ src/voiceModule/voice.service.ts
@@ -1,3 +1,9 @@
 export class VoiceService {
 
+    // Ok, l'idée c'est former une url pour translate.google.com avec le text, les langues et tout.
+    // Ensuite, avec Puppeteer on record la video, on clique sur le bouton du haut parleur
+    // On arrête l'enregistrement
+    // On prends juste le son de la video
+    // On renvoie le son sous le bon format en tant que res
+
 }
